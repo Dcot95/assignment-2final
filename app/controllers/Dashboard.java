5642f84be21e401874053a8f3a5686d5359c9905
David Cotter <20062924@mail.wit.ie>
2022-05-28 14:45:23 +0100

    ability to add and delete a station

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -18,4 +18,22 @@ public class Dashboard extends Controller {
         render("dashboard.html", stations);
     }
 
+    public static void deleteStation(Long id) {
+        Logger.info("Deleting a Station");
+        Member member = Accounts.getLoggedInMember();
+        Station station = Station.findById(id);
+        member.stations.remove(station);
+        member.save();
+        station.delete();
+        redirect("/dashboard");
+    }
+
+    public static void addStation(String name) {
+        Logger.info("Adding a Station");
+        Member member = Accounts.getLoggedInMember();
+        Station station = new Station(name);
+        member.stations.add(station);
+        station.save();
+        redirect("/dashboard");
+    }
 }
