390f625d9de571c95a22eb2862cdd4f3139dc54b
David Cotter <20062924@mail.wit.ie>
2022-05-28 14:32:46 +0100

    stations listed

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jdk.internal.net.http.common.Log;
+import models.Member;
 import models.Station;
 import models.Reading;
 import play.Logger;
@@ -12,7 +13,9 @@ import play.mvc.Controller;
 public class Dashboard extends Controller {
     public static void index() {
         Logger.info("Rendering Dashboard");
-        render("dashboard.html");
+        Member member = Accounts.getLoggedInMember();
+        List<Station> stations = member.stations;
+        render("dashboard.html", stations);
     }
 
 }
